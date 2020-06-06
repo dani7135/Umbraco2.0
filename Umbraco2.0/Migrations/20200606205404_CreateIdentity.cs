d40e3a6d775f12daa53c9fae89b1ac3db181867f
Daniella Clemmensen <dani7135@edu.ucl.dk>
2020-06-06 22:57:42 +0200

    Test

---
+++ Umbraco2.0/Migrations/20200606205404_CreateIdentity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Umbraco2._0.Migrations
+{
+    public partial class CreateIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
